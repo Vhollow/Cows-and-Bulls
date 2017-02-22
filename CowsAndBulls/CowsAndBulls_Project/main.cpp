769d5817f95db7bcccd86d3bbb3cf9b42cc2c2a3
Vicente Martínez Franco <vicentemartinezf@gmail.com>
2017-02-22 19:03:12 +0100

    Gets a word from player

--- CowsAndBulls/CowsAndBulls_Project/main.cpp
+++ CowsAndBulls/CowsAndBulls_Project/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,7 +7,14 @@ int main() {
 	constexpr int WORD_LENGTH = 6;
 
 	cout << "Welcome to Bulls and Cows.\n";
-	cout << "In this game you have to guess a "<< WORD_LENGTH << " letter length isogram"<< endl;
+	cout << "In this game you have to guess a "<< WORD_LENGTH << " letter length isogram\n"<< endl;
+
+	// Get a guess from player
+	string Guess = "";
+	cout << "Make a guess:";
+	cin >> Guess;
+	//Repeat back
+	cout << "Your guess is:" << Guess << endl;
 
 	return 0;
 }
