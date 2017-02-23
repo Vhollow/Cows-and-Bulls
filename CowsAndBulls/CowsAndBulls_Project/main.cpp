b59af0c63f58acd881e178eb9dc47b33f3054019
Vicente Martínez Franco <vicentemartinezf@gmail.com>
2017-02-23 12:04:56 +0100

    Putting code into functions

--- CowsAndBulls/CowsAndBulls_Project/main.cpp
+++ CowsAndBulls/CowsAndBulls_Project/main.cpp
@@ -3,18 +3,28 @@
 
 using namespace std;
 
-int main() {
-	constexpr int WORD_LENGTH = 6;
+constexpr int WORD_LENGTH = 6;
+string Guess = "";
 
+void PrintIntro() {
+	//Print the introduction lines
 	cout << "Welcome to Bulls and Cows.\n";
-	cout << "In this game you have to guess a "<< WORD_LENGTH << " letter length isogram\n"<< endl;
-
+	cout << "In this game you have to guess a " << WORD_LENGTH << " letter length isogram\n" << endl;
+	return;
+}
+void GetGuess() {
 	// Get a guess from player
-	string Guess = "";
 	cout << "Make a guess:";
-	getline(cin , Guess);
+	getline(cin, Guess);
 	//Repeat back
 	cout << "Your guess is:" << Guess << endl;
+	return;
+}
+
+int main() {
+
+	PrintIntro();
+	GetGuess();
 
 	return 0;
 }
